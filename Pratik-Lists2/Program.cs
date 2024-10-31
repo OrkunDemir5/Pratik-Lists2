8732f5a80a02f4767a938eb3a7a033c440fbf62e
OrkunDemir5 <orkun.d1999@gmail.com>
2024-10-31 15:04:20 +0300

    Pratik-Lists2

---
+++ Pratik-Lists2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+// Kahve isimlerini saklamak için bir List<string> oluşturuluyor.
+List<string> kahveler = new List<string>();
+
+// Kullanıcıdan 5 kahve ismi girmesini istiyoruz.
+for (int i = 1; i < 6; i++)
+{
+    Console.Write($"Kahve {i}: ");
+    
+    string KahveAdi = Console.ReadLine();
+
+    // Kullanıcının girdiği kahve ismini listeye ekliyoruz.
+    kahveler.Add( KahveAdi );
+}
+
+Console.WriteLine("Girdiğiniz Kahveler");
+
+int siralama = 1;
+// Listeyi foreach döngüsüyle ekrana yazdırıyoruz.
+foreach ( string kahve in kahveler)
+{
+    Console.WriteLine($"{siralama}. " + kahve);
+
+    siralama++;
+}
